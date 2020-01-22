306d5fbc87cd0234f7a648280e063f3c93f6c5ab
Stefan Terdell <stefan.terdell@live.com>
2020-01-22 18:33:39 +0100

    :sparkles:

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const mustacheExpress = require('mustache-express')()
-const port =  process.env.LOCALHOST_PORT || (process.argv[2] === '-p' ? process.argv[3] : 3000)
-const path = (folder) => require('path').join(__dirname, '..', folder)
+const port =  process.env.LOCALHOST_PORT || process.argv[2] || 3000
+const path = require('path')
 
 const app = express()
 
 app.engine('mst', mustacheExpress)
 app.set('view engine', 'mst')
-app.set('views', path('views'))
-app.use('/static', express.static(path('static')));
+app.set('views', path.join(__dirname, '..', 'views'))
+app.use('/static', express.static(path.join(__dirname, '..', 'static')));
 
 app.get('/', (req, res) => {
     res.render('index', { myContent: "Hello world!", myMessage: "Hello console!" })
